26dd4437122213b0f26933e9920b8ebca302fa7e
lahin03 <lahinsaleem03@gmail.com>
2025-05-30 23:11:35 +0530

    initial commit

---
+++ bitespeed_assg-1/src/main/java/com/bitespeed/BitespeedAssg1Application.java
@@ -0,0 +1,13 @@
+package com.bitespeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitespeedAssg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitespeedAssg1Application.class, args);
+	}
+
+}
